4f574a25959bdf53b9207765391ca9cbad35d026
Demjanenko, Alexej <alexej.demjanenko@arithnea.de>
2020-01-25 10:46:24 +0100

    config optimization

--- src/index.js
+++ src/index.js
@@ -4,17 +4,6 @@ import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
-window.alertConfig = {
-    // API_URL: 'https://my-json-server.typicode.com/fritzfde/tr/alerts',
-    // API_URL: 'http://85.22.63.157:3000/alerts',
-    // API_URL: 'http://localhost:9001/jsonData/alerts.json',
-    API_URL: 'http://localhost:3000/alerts',
-    USERNAME: 'TuR',
-    PASSWORD: 'A4TuR2019',
-    DELAY: 5000,
-    DEBUG: true
-}
-
 ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
